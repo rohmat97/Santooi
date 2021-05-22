939199cdb587b6c4e1a1f316bcee873229f22600
Rohmat Dasuki <rohmat661@gmail.com>
2021-05-22 12:08:29 +0700

    fixing platform android

--- android/app/src/main/java/com/vodjo/surface/MainActivity.java
+++ android/app/src/main/java/com/vodjo/surface/MainActivity.java
@@ -21,7 +21,7 @@ public class MainActivity extends ReactActivity {
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
-      SplashScreen.hide();
+      // SplashScreen.hide(this);
       super.onCreate(savedInstanceState);
   }
 
