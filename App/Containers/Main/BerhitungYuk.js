8ee16ca031d354f45a6fd1681402f8cb4d1bc115
Rohmat Dasuki <rohmat661@gmail.com>
2021-05-30 03:40:56 +0700

    update timer

--- App/Containers/Main/BerhitungYuk.js
+++ App/Containers/Main/BerhitungYuk.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import { ScrollView, View, Image, Text, TouchableOpacity } from 'react-native'
 import { TemplateBackground } from '../../Components/TemplateBackground'
 import images from '../../Themes/Images';
@@ -7,14 +7,56 @@ import Colors from '../../Themes/Colors'
 import { Screen } from '../../Transforms/Screen'
 import { connect } from 'react-redux';
 import { OverlayBerhitung } from '../../Components/OverlayBerhitung';
+import { ImageBackground } from 'react-native';
 
 function BerhitungYuk(props) {
     const { navigation } = props
     const { pop } = navigation
     const [visible, setVisible] = useState(false);
+    const [Minute, setMinute] = useState(0);
+    const [Second, setSecond] = useState(0);
+    const [start, setstart] = useState(false);
+    const [reset, setreset] = useState(false);
     const toggleOverlay = () => {
         setVisible(!visible);
     };
+    const timerRef = useRef(null);
+
+    useEffect(()=>{
+        // console.log(currentDate.getHours()+":"+currentDate.getMinutes()+":"+currentDate.getSeconds())
+      
+          return () => {
+            if (timerRef.current) {
+              clearTimeout(timerRef.current);
+            }
+          };
+    },[])
+    useEffect(()=>{
+        if(start){
+            // console.log(Second.charAt(0))
+            timerRef.current = setTimeout(() => {
+                if(Second>59){
+                    setSecond('00')
+                    setMinute(parseInt(Minute)+1)
+                }else{
+                    if(Second.length>1){
+                        setSecond((parseInt(Second)+1).toString())
+                    }else{
+                    let parsing =(parseInt(Second)+1)
+                    setSecond(parsing)
+                    }
+                }
+            }, 1000);
+        }else{
+            if(reset){
+                clearTimeout(timerRef.current)
+                setreset(false)
+                setSecond(0)
+                setMinute(0)
+            }
+        }
+        
+    },[start,reset,Second])
 
     return (
         <TemplateBackground cover={true}>
@@ -35,9 +77,25 @@ function BerhitungYuk(props) {
                         </TouchableOpacity>
                     </View>
                     <ScrollView>
-                        <Text style={{ color: '#67308F', fontSize: 130, textAlign: 'center', marginVertical: Screen.width * 0.13 }}>00:30</Text>
-                        <Image source={images.start} style={{ width: Screen.width * 0.7, height: Screen.width * 0.5, alignSelf: 'center' }} resizeMode='contain' />
-                        <Image source={images.reset} style={{ width: Screen.width * 0.5, height: Screen.width * 0.18, alignSelf: 'center', marginVertical: Screen.width * 0.1 }} resizeMode='contain' />
+                        <Text style={{ color: '#67308F', fontSize: 130, textAlign: 'center', marginVertical: Screen.width * 0.13 }} numberOfLines={1}>
+                                <Text>{Minute>9?Minute:'0'+Minute}</Text>
+                                <Text>:</Text>
+                                <Text>{Second>9?Second:'0'+Second }</Text>
+                        </Text>
+                        <TouchableOpacity onPress={()=> setstart(!start)}>
+                        <View style={{width:Screen.width, alignSelf: 'center',justifyContent:'center'}}>
+                            <ImageBackground source={images.start} style={{maxWidth:205, maxHeight:205, width:Screen.width*0.45,height:Screen.height*0.3, alignSelf: 'center',justifyContent:'center' }}>
+                            
+                                    <Text style={{color:'#67308F', fontSize:39, fontWeight:'100',textAlign:'center'}}>START</Text>
+                            </ImageBackground>
+                            </View>
+                        </TouchableOpacity>
+                        <TouchableOpacity onPress={()=>{
+                            setstart(false)
+                            setreset(true)
+                        }}>
+                            <Image source={images.reset} style={{ width: Screen.width * 0.5, height: Screen.width * 0.18, alignSelf: 'center', marginVertical: Screen.width * 0.1 }} resizeMode='contain' />
+                        </TouchableOpacity>
                         <OverlayBerhitung visible={visible} toggleOverlay={toggleOverlay} />
                     </ScrollView>
                 </View>
