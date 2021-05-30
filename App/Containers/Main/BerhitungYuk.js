f9515bd87acf75d55c8734e881211b875cee90a4
Rohmat Dasuki <rohmat661@gmail.com>
2021-05-31 01:25:54 +0700

    set music at layout berhitung, set function when start counting ,fungsion reset counter, play music when counter run , music default off if choose music when start counting the music will play, the music still dummy

--- App/Containers/Main/BerhitungYuk.js
+++ App/Containers/Main/BerhitungYuk.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useRef, useState } from 'react'
 import { ScrollView, View, Image, Text, TouchableOpacity } from 'react-native'
+import SoundPlayer from 'react-native-sound-player'
 import { TemplateBackground } from '../../Components/TemplateBackground'
 import images from '../../Themes/Images';
 import styles from '../Styles/LaunchScreenStyles'
@@ -17,18 +18,66 @@ function BerhitungYuk(props) {
     const [Second, setSecond] = useState(0);
     const [start, setstart] = useState(false);
     const [reset, setreset] = useState(false);
+    const [music, setMusic] = useState()
+    const [pemandu, setPemandu] = useState('')
+    const [duration, setduration] = useState(0)
     const toggleOverlay = () => {
         setVisible(!visible);
     };
     const timerRef = useRef(null);
 
+    const Start =(payload) =>{
+        setstart(payload)
+        if(music && payload){
+            playSound()
+            getInfo()
+        }else{
+            SoundPlayer.pause()
+        }
+    }
+    const playSound =()=>{
+        try {
+            // or play from url
+            SoundPlayer.playUrl('https://file-examples-com.github.io/uploads/2017/11/file_example_MP3_700KB.mp3')
+        } catch (e) {
+            // console.log(`cannot play the sound file`, e)
+        }
+    }
+    const getInfo = async()=> { // You need the keyword `async`
+        try {
+          const info = await SoundPlayer.getInfo() 
+          setduration(info.duration)
+        } catch (e) {
+        //   console.log('There is no song playing', e)
+        }
+      }
+    let _onFinishedPlayingSubscription = null
+    let _onFinishedLoadingSubscription = null
+    let _onFinishedLoadingFileSubscription = null
+    let _onFinishedLoadingURLSubscription = null
     useEffect(()=>{
         // console.log(currentDate.getHours()+":"+currentDate.getMinutes()+":"+currentDate.getSeconds())
-      
+        _onFinishedPlayingSubscription = SoundPlayer.addEventListener('FinishedPlaying', ({ success }) => {
+            console.log('finished playing', success)
+            playSound()
+          })
+          _onFinishedLoadingSubscription = SoundPlayer.addEventListener('FinishedLoading', ({ success }) => {
+            console.log('finished loading', success)
+          })
+          _onFinishedLoadingFileSubscription = SoundPlayer.addEventListener('FinishedLoadingFile', ({ success, name, type }) => {
+            console.log('finished loading file', success, name, type)
+          })
+          _onFinishedLoadingURLSubscription = SoundPlayer.addEventListener('FinishedLoadingURL', ({ success, url }) => {
+            console.log('finished loading url', success, url)
+          })
           return () => {
             if (timerRef.current) {
               clearTimeout(timerRef.current);
             }
+            _onFinishedPlayingSubscription.remove()
+            _onFinishedLoadingSubscription.remove()
+            _onFinishedLoadingURLSubscription.remove()
+            _onFinishedLoadingFileSubscription.remove()
           };
     },[])
     useEffect(()=>{
@@ -36,7 +85,7 @@ function BerhitungYuk(props) {
             // console.log(Second.charAt(0))
             timerRef.current = setTimeout(() => {
                 if(Second>59){
-                    setSecond('00')
+                    setSecond(0)
                     setMinute(parseInt(Minute)+1)
                 }else{
                     if(Second.length>1){
@@ -46,6 +95,9 @@ function BerhitungYuk(props) {
                     setSecond(parsing)
                     }
                 }
+                if(duration>0){
+                    setduration(duration-1)
+                }
             }, 1000);
         }else{
             if(reset){
@@ -53,6 +105,7 @@ function BerhitungYuk(props) {
                 setreset(false)
                 setSecond(0)
                 setMinute(0)
+                SoundPlayer.stop()
             }
         }
         
@@ -77,17 +130,16 @@ function BerhitungYuk(props) {
                         </TouchableOpacity>
                     </View>
                     <ScrollView>
-                        <Text style={{ color: '#67308F', fontSize: 130, textAlign: 'center', marginVertical: Screen.width * 0.13 }} numberOfLines={1}>
+                        <Text style={{ color: '#67308F', fontSize: 110, textAlign: 'center', marginVertical: Screen.width * 0.13 }} numberOfLines={1}>
                                 <Text>{Minute>9?Minute:'0'+Minute}</Text>
                                 <Text>:</Text>
                                 <Text>{Second>9?Second:'0'+Second }</Text>
                         </Text>
-                        <TouchableOpacity onPress={()=> setstart(!start)}>
-                        <View style={{width:Screen.width, alignSelf: 'center',justifyContent:'center'}}>
-                            <ImageBackground source={images.start} style={{maxWidth:205, maxHeight:205, width:Screen.width*0.45,height:Screen.height*0.3, alignSelf: 'center',justifyContent:'center' }}>
-                            
+                        <TouchableOpacity onPress={()=> Start(!start)}>
+                            <View style={{width:Screen.width, alignSelf: 'center',justifyContent:'center'}}>
+                                <ImageBackground source={images.start} style={{maxWidth:205, maxHeight:205, width:Screen.width*0.45,height:Screen.height*0.3, alignSelf: 'center',justifyContent:'center' }}>
                                     <Text style={{color:'#67308F', fontSize:39, fontWeight:'100',textAlign:'center'}}>START</Text>
-                            </ImageBackground>
+                                </ImageBackground>
                             </View>
                         </TouchableOpacity>
                         <TouchableOpacity onPress={()=>{
@@ -96,7 +148,7 @@ function BerhitungYuk(props) {
                         }}>
                             <Image source={images.reset} style={{ width: Screen.width * 0.5, height: Screen.width * 0.18, alignSelf: 'center', marginVertical: Screen.width * 0.1 }} resizeMode='contain' />
                         </TouchableOpacity>
-                        <OverlayBerhitung visible={visible} toggleOverlay={toggleOverlay} />
+                        <OverlayBerhitung visible={visible} toggleOverlay={toggleOverlay} music={music} setMusic={setMusic} pemandu={pemandu} setPemandu={setPemandu} />
                     </ScrollView>
                 </View>
 
